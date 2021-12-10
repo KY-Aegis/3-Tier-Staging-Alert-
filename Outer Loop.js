7715be1b7485901d4ebf1156c0597a0ad2cf2032
KheeYaw <79307366+KY-Aegis@users.noreply.github.com>
2021-12-10 15:42:51 +0800

    Updates

---
+++ Outer Loop.js
@@ -0,0 +1,8 @@
+# Start your code below, you can access parameter values as normal list starting from index 1 e.g. i = parameters[1], you can write to output value as normal list starting from index 1 e.g. output[1]= 1+1
+import json
+output[1]=[{"i1": "4", "i2":"Level 3"},
+           {"i1": "1", "i2":"Level 2"},
+           {"i1": "0.5", "i2":"Level 1"},
+      
+]
+# end your code here #
